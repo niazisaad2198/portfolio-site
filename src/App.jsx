fa627df7833e024d51161d1504f58d2d8bd54b0b
niazisaad2198 <niazisaad2198@gmail.com>
2022-08-28 07:46:03 +0500

    Added Navbar. (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
-
-function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <div className=" h-screen bg-gradient-to-b from-darker to-light">
+      <div
+        id="navbar"
+        className=" grid grid-cols-4 justify-around py-4 bg-light h-16"
+      >
+        <h1 className=" ml-4 col-span-1 text-white font-mono text-lg font-semibold">
+          N-Tech
+        </h1>
+        <div id="nav-links" className=" flex justify-end gap-x-4 col-start-3 col-span-2 pr-4">
+          <a href="#">Home</a>
+          <a href="#">About Us</a>
+          <a href="#">Contact Us</a>
+        </div>
+      </div>
+      <div id="content" className="text-white">
+        content
+      </div>
+    </div>
+  );
+};
 
 export default App;
